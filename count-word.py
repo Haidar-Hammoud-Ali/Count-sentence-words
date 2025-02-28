f442aaa45c7e71cfeecee1f56937f92ae4ab0b58
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-28 12:22:59 +0200

    Creating a word dictionary

--- count-word.py
+++ count-word.py
@@ -1,3 +1,8 @@
 sentence= input ("Enter a sentence: ")
 words= sentence.split ()
 print (words)
+
+word_dict = {}
+for word in words:
+    word_dict [word]= word_dict.get (word, 0)+1
+print (word_dict)
