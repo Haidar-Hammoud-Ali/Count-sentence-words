8d3674ae3fd9b4c326fe052a3619d5c5c2522cb6
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-28 12:38:53 +0200

    Defining word count function

--- count-word.py
+++ count-word.py
@@ -6,3 +6,14 @@ word_dict = {}
 for word in words:
     word_dict [word]= word_dict.get (word, 0)+1
 print (word_dict)
+
+def word_dictf (sentence):
+    words= sentence.split ()
+
+    word_dictf={}
+    for word in words:
+        word_dictf [word]= word_dictf.get (word, 0)+1
+    return (word_dictf)
+
+result=word_dictf ("A B B N N N M M M M N  B BB B B B" )
+print (result)
